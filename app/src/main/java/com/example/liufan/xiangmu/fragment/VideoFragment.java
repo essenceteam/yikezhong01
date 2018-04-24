be68470b74fab6ed8db6ef6c052253108e5ccbbd
liutian941013 <811308544@qq.com>
2018-04-24 22:05:11 +0800

    11111

--- app/src/main/java/com/example/liufan/xiangmu/fragment/VideoFragment.java
+++ app/src/main/java/com/example/liufan/xiangmu/fragment/VideoFragment.java
@@ -2,7 +2,9 @@ package com.example.liufan.xiangmu.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +16,19 @@ import com.example.liufan.xiangmu.R;
  */
 
 public class VideoFragment extends Fragment {
+
+    private TabLayout video_tab;
+    private ViewPager video_vp;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = View.inflate(getActivity(), R.layout.video_frag_view, null);
+        video_tab = view.findViewById(R.id.video_tab);
+        video_vp = view.findViewById(R.id.video_vp);
+//        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter();
+//        video_vp.setAdapter();
+
         return view;
     }
 }
