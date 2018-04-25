1b4678d36999d285aeceaabd8ddd60bd9bdca214
cuitengfei <17600561292@163.com>
2018-04-25 19:57:18 +0800

    444

--- app/src/main/java/com/example/liufan/xiangmu/duanzi/presenter/Presenter.java
+++ app/src/main/java/com/example/liufan/xiangmu/duanzi/presenter/Presenter.java
@@ -1,10 +1,13 @@
 package com.example.liufan.xiangmu.duanzi.presenter;
 
+import com.example.liufan.xiangmu.duanzi.modle.ModelSF;
+
 /**
  * Created by liufan on 2018/4/24.
  */
 
 public interface Presenter {
 
+    void getDZData(String url,int page ,ModelSF modelSF);
 
 }
