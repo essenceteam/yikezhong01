0722bc944a2df5551c3cfcb1eb351a69f810c46a
Shishuaifeng <1362388966@qq.com>
2018-04-25 11:50:15 +0800

    时帅丰02

---
+++ app/src/main/java/com/example/liufan/xiangmu/tuijain/Remen_Fragment.java
@@ -0,0 +1,20 @@
+package com.example.liufan.xiangmu.tuijain;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lenovo on 2018/4/25.
+ */
+
+public class Remen_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView (inflater, container, savedInstanceState);
+    }
+}
