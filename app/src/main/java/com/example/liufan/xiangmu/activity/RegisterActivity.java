3c77e1307c4f195368a34f73acaf44f2fc71593e
liutian941013 <811308544@qq.com>
2018-04-25 20:22:24 +0800

    444

---
+++ app/src/main/java/com/example/liufan/xiangmu/activity/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.liufan.xiangmu.activity;
+import com.example.liufan.xiangmu.baes.BaesActivity;
+
+public class RegisterActivity extends BaesActivity {
+
+
+    @Override
+    public int getlayout() {
+        return 0;
+    }
+
+    @Override
+    public void getinit() {
+
+    }
+
+    @Override
+    public void getdata() {
+
+    }
+}
