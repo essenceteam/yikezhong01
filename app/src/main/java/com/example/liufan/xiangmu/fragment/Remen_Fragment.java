f973b40b64bebf9dac6e787f409e34913cd785b0
wukequdai <1362388966@qq.com>
2018-04-27 15:29:46 +0800

    时帅丰08

--- app/src/main/java/com/example/liufan/xiangmu/fragment/Remen_Fragment.java
+++ app/src/main/java/com/example/liufan/xiangmu/fragment/Remen_Fragment.java
@@ -11,9 +11,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.liufan.xiangmu.R;
-import com.example.liufan.xiangmu.adapter.TuiJian_ReMen_Baseadapter;
+import com.example.liufan.xiangmu.adapter.Tuijian_Video_baseadapter;
+import com.example.liufan.xiangmu.tuijain.View.IShiPinView;
 import com.example.liufan.xiangmu.tuijain.View.ItuijianView;
 import com.example.liufan.xiangmu.tuijain.modle.Bean.LunBoBean;
+import com.example.liufan.xiangmu.tuijain.modle.Bean.TuijianshipinBean;
 import com.example.liufan.xiangmu.tuijain.presenter.Presenter;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 import com.squareup.picasso.Picasso;
@@ -35,10 +37,11 @@ public class Remen_Fragment extends Fragment {
     @BindView(R.id.remenrev1)
     XRecyclerView remenrev1;
     Unbinder unbinder;
+    @BindView(R.id.bann)
+    Banner bann;
     private View view;
-    private View inflate;
-    private Banner bann;
-    private com.example.liufan.xiangmu.tuijain.presenter.Presenter presenter;
+    private int page =1;
+    Presenter Presenter = new Presenter ();
     List<String> list_tu = new ArrayList<> ();
 
     @Nullable
@@ -46,21 +49,6 @@ public class Remen_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate (R.layout.tuijian_remen_layout, container, false);
         unbinder = ButterKnife.bind (this, view);
-        inflate = View.inflate (getActivity (), R.layout.bann_layout, null);
-        remenrev1.addHeaderView (inflate);
-        bann = inflate.findViewById (R.id.bann);
-        getlunbo();
-        getdata1();
-        //TuiJian_ReMen_Baseadapter TuiJian_ReMen_Baseadapter=new TuiJian_ReMen_Baseadapter ();
-        //remenrev1.setAdapter (TuiJian_ReMen_Baseadapter);
-
-        TuiJian_ReMen_Baseadapter TuiJian_ReMen_Baseadapter=new TuiJian_ReMen_Baseadapter ();
-        remenrev1.setAdapter (TuiJian_ReMen_Baseadapter);
-
-        return view;
-    }
-    //上啦刷新，下拉加载
-    private void getdata1() {
         remenrev1.setLayoutManager (new LinearLayoutManager (getActivity ()));
         remenrev1.setLoadingListener (new XRecyclerView.LoadingListener () {
             @Override
@@ -73,6 +61,32 @@ public class Remen_Fragment extends Fragment {
 
             }
         });
+        getlunbo ();
+        getdata1 ();
+        return view;
+    }
+
+    //上啦刷新，下拉加载
+    private void getdata1() {
+       Presenter.getvrdiopresenter ("android", page, new IShiPinView () {
+
+           private List<TuijianshipinBean.DataBean> data;
+
+           @Override
+           public void TuijianShiPinOnSuccess(TuijianshipinBean TuijianshipinBean) {
+               data = TuijianshipinBean.getData ();
+               Tuijian_Video_baseadapter Tuijian_Video_baseadapter=new Tuijian_Video_baseadapter (getActivity (),data);
+               remenrev1.setAdapter (Tuijian_Video_baseadapter);
+
+
+
+           }
+
+           @Override
+           public void TuijianShiPinOnError(Throwable Throwable) {
+
+           }
+       });
     }
 
     @Override
@@ -80,15 +94,17 @@ public class Remen_Fragment extends Fragment {
         super.onDestroyView ();
         unbinder.unbind ();
     }
+
     //轮播图
-    public void getlunbo(){
-        Presenter Presenter=new Presenter ();
-        Presenter.getlunbopresenter (new ItuijianView() {
+    public void getlunbo() {
+
+        Presenter.getlunbopresenter (new ItuijianView () {
             private List<LunBoBean.DataBean> data;
+
             @Override
             public void TuijianOnSuccess(LunBoBean LunBoBean) {
                 data = LunBoBean.getData ();
-                for (int i = 0; i <data.size () ; i++) {
+                for (int i = 0; i < data.size (); i++) {
                     String icon = data.get (i).getIcon ();
                     list_tu.add (icon);
                 }
